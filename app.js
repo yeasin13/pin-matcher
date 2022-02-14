5add8ee0a442daed38901eea22b28eb5bb5da734
yeasin13 <yeasinarafat8001@gmail.com>
2022-02-14 15:46:35 +0600

    first-condition

--- app.js
+++ app.js
@@ -13,3 +13,19 @@ function generatePin(){
     const pin = getPin();
     document.getElementById('display-input').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click',function(event){
+    const number = event.target.innerText;
+    const calcInput= document.getElementById('typed-numbers');
+    if(isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+   else{
+    
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+   }
+});
