41f69b610c8e952d1f08ed9e4b100810f1de4fd2
yeasin13 <yeasinarafat8001@gmail.com>
2022-02-14 14:17:59 +0600

    pin-done

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+function getPin(){
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if(pinString.length == 4){
+        return pin;
+    }
+    else{
+        console
+        return getPin;
+    }
+}
 function generatePin(){
-    const pin = Math.round(Math.random()*10000)
-    console.log(pin);
+    const pin = getPin();
+    document.getElementById('display-input').value = pin;
 }
